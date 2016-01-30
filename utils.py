dd3e904898bd21e1288c211b7ae8e13735041591
Karan Kamath <karankamath26@gmail.com>
2016-01-31 04:08:25 +0800

    Fix bugs, refactor

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-def get_input(f, lines_per_case, transforms):
-    numCases = int(f.readline())
+def get_input(f, num_cases, lines_per_case, transforms):
     
     case_inputs = []
 
-    for case in range(numCases):
+    for case in range(num_cases):
         case_input = []
 
         for i in range(lines_per_case):
@@ -11,7 +10,7 @@ def get_input(f, lines_per_case, transforms):
 
         case_inputs.append(case_input)
 
-    return (numCases, case_inputs)
+    return case_inputs
 
 def get_output_string(case, ans):
     return 'Case #' + str(case) + ': ' + str(ans) + '\n'
@@ -21,14 +20,14 @@ def print_output(fw, numCases, case_inputs, get_ans):
         fw.write(get_output_string(case, get_ans(case_inputs[case-1])))
 
 def process(fname, num_lines, transforms, get_ans):
-"""Process the input to calculate and write out the correct output
-
-fname --  The name of the input file
-num_lines -- The number of lines PER case
-transforms -- The transforms to apply to each line of a case
-get_ans -- The method that takes a case as an input and returns the output for the case
-"""
-   
+    """Process the input to calculate and write out the correct output
+    
+    fname --  The name of the input file
+    num_lines -- The number of lines PER case
+    transforms -- The transforms to apply to each line of a case
+    get_ans -- The method that takes a case as an input and returns the output for the case
+    """
     with open(fname) as fr, open(fname + '.out', 'w') as fw:
-        numCases, case_inputs = get_input(fr, num_lines, transforms)
-        print_output(fw, numCases, case_inputs, get_ans)
+        num_cases = int(fr.readline().strip())
+        case_inputs = get_input(fr, num_cases, num_lines, transforms)
+        print_output(fw, num_cases, case_inputs, get_ans)
