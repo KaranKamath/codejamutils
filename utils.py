7846fea75dd64e30253a60748f69f4f1d9191c8b
Karan Kamath <karankamath26@gmail.com>
2016-01-31 03:31:25 +0800

    Add docstring

--- utils.py
+++ utils.py
@@ -21,6 +21,14 @@ def print_output(fw, numCases, case_inputs, get_ans):
         fw.write(get_output_string(case, get_ans(case_inputs[case-1])))
 
 def process(fname, num_lines, transforms, get_ans):
+"""Process the input to calculate and write out the correct output
+
+fname --  The name of the input file
+num_lines -- The number of lines PER case
+transforms -- The transforms to apply to each line of a case
+get_ans -- The method that takes a case as an input and returns the output for the case
+"""
+   
     with open(fname) as fr, open(fname + '.out', 'w') as fw:
         numCases, case_inputs = get_input(fr, num_lines, transforms)
         print_output(fw, numCases, case_inputs, get_ans)
